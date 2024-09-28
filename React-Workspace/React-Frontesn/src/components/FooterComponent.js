64903372f24a47127557b2c1f9e3542b09326805
tejaspawar178 <tejaspawar329@gmail.com>
2024-09-29 00:52:33 +0530

    Project Push Successfully

---
+++ React-Workspace/React-Frontesn/src/components/FooterComponent.js
@@ -0,0 +1,17 @@
+import React,{Component}from 'react'
+import '../App.css';
+
+	export default class FooterComponent extends Component {
+ 	 render() {
+    	return (
+            
+      	<div>
+        	<footer className='footer' security='last'>
+       <span>All Right Reserved 2024 &copy;PAWAR_TEJAS </span>
+        </footer>
+     	 </div>
+        
+    	)
+  	}
+	}
+
---
+++ React-Workspace/React-Frontesn/src/components/HeaderComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+	export default class HeaderComponent extends Component {
+  	render() {
+    	return (
+      	<div>
+            	
+                <nav className='navbar navbar-expand-lg bg-dark navbar-dark'>
+                	    <div className='container'>
+          <a href="" className='navbar-brand '>Employee Management Application</a>
+                    	</div>
+                	</nav>
+         	   
+      	</div>
+    	)
+  	}
+	}   
